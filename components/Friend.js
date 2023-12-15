315378f7033e68ad2844971cb66aee85aaef669c
OnurKeles4 <onurkeles2002@hotmail.com>
2023-12-14 19:59:17 -0700

    Added new Features to Friends
    FriendsPage new have a adding a friend option and seeing more details about friend when clicked on their profile picture

--- components/Friend.js
+++ components/Friend.js
@@ -4,22 +4,76 @@ import ImageButton from './ImageButton.js';
 import {Text, View, VirtualizedList} from 'react-native';
 
 const Friend = (props) => {
-  const renderItem = ({ item }) => {
-       return (
-         <View  style={{
-              flex: 1,
-              
-              flexDirection: 'column',
-              margin: 5
-            }}>
-        <ImageButton image={item.image} label={item.username} click={seeFriendProfile}> </ImageButton>
-         </View>
-    );
- };
- //WIP See detailed information about friend's profile 
- function seeFriendProfile() {
+  const renderItem = ({ item, index}) => {
+    return (
+      <View  style={{
+           flex: 1,
+           
+           flexDirection: 'column',
+           margin: 5
+         }}>
+     <ImageButton image={item.image} label={item.username} click={()=>seeFriendProfile(item)}> </ImageButton>
+      </View>
+ );
+};
+
+ const renderStats = () => {
+  if (showStats) {
+    var allViewed = "";
+    var allRated = "";
+    console.log("Viewed:");
+    console.log(viewed);
+    
+    console.log("Rated:");
+    console.log(rates);
+    for(i = 0; i < viewed.length; i++) {
+      allViewed += viewed[i].Title + ", ";
+    }
+    allViewed = allViewed.slice(0, allViewed.length - 2);
+
+    for(i = 0; i < rates.length; i++) {
+      allRated += rates[i].Title + ", ";
+    }
+    allRated = allRated.slice(0, allRated.length - 2);
+      return (
+          <View style={styles.container}>
+              <Text style={styles.smallPhotoText}>User Statistics:</Text>
+              <Text>Viewed: {allViewed }</Text>
+              <Text>Rated: {allRated}</Text>
+          </View>
+      );
+  }
+  return null;
+};
+  async function seeFriendProfile(item) {
+   console.log(item);
+    const response = await fetch(props.aURL);
+   if (response.ok) {
+                const names = await response.json();
+                console.log("Searching for a friend " + item.username);
+                const userFound = names.find(userJSON => userJSON.username === item.username);
+                if(userFound)
+                console.log("searching found: " + userFound.username);
+                else {
+                  console.log("user couldn't found, this person doesn't have any info");
+                  return;
+                }
+                console.log("User Found is: ");
+                console.log(userFound);
+
+                setViewed(userFound.viewed);
+                setRates(userFound.rates);
+ 
+   }
+  
+   //look for a user with this username, then get their movie information
+   //user = for loop: props.userdatabase[i].username == item.username
+   //set viewed with the user data setViewed(user.viewed)
+   //set rating with the user data setViewed(user.rates)
    console.log("Page navigated to friend's profile");
+   setShowStats(!showStats);
  }
+ 
  
 
  var preview= <View style={styles.container} >
--- routes/FriendsPage.js
+++ routes/FriendsPage.js
@@ -11,28 +11,120 @@ const VirtualListBasics = ({user}) => {
   const [aphoto, setPhoto] = useState('assets/snack-icon.png');
 
   //Get our user information to access their friends. 
-useEffect(() => {
-        async function loadList(url) {
 
-            const response = await fetch(url);
-            if (response.ok) {
-                const names = await response.json();
+  useEffect(() => {
+    async function loadList(url) {
+       
+         const response = await fetch(url);
+        if (response.ok) {
+            const names = await response.json();
 
-                const foundUser = names.find(JSONuser => JSONuser.username === user.username);
-                if (foundUser)
-                    setFriends(foundUser.friends);
+            const foundUser = names.find(JSONuser => JSONuser.username === user.username);
+            console.log("founduser:");
+            console.log(foundUser);
+            if (foundUser) {
+                setFriends(foundUser.friends);
+                setaUser(foundUser);
             }
+            else 
+              console.log("Problem");
         }
-            const urladress = 'https://cs.boisestate.edu/~scutchin/cs402/codesnips/loadjson.php?user={movierater}';
-
-        loadList(urladress)
+    }
+      
+    loadList(urladress)
 
 }, []);
 
 
+const dialogInp =  <DialogInput isDialogVisible={showme} 
+      title="Enter username"
+      message="Enter The your friends username to look for them"
+      submitInput={ (inputText) =>{setShowme(false); searchforFriend(inputText)}}
+      closeDialog={() => {setShowme(false)}}
+      >
+      <Text>Something</Text>
+      </DialogInput>;
+
+async function searchforFriend(inputText) {
+//use the url to get all users
+const response = await fetch(urladress);
+        if (response.ok) {
+            const names = await response.json();
+            console.log("Searching for a friend " + inputText);
+            //compare the given username from database
+            const userFound = names.find(userJSON => userJSON.username === inputText);
+            if(userFound)
+            console.log("searching found: " + userFound.username);
+            else {
+              console.log("user couldn't found, this person DNE");
+              return; //if person DNE, return
+            }
+
+            //This part checks if this person is already a friend, if that's the case, don't add the user again.
+            
+            var isAlreadyFriend;
+            for(i = 0; i < aUser.friends.length; i++) {
+              console.log("username: and length")
+                console.log(aUser.friends[i].username);
+                console.log(aUser.friends.length);
+              if(aUser.friends[i].username === inputText) {
+                
+                isAlreadyFriend = 1;
+                break;
+              }
+              else {
+                isAlreadyFriend = 0;
+              }
+            }
+            console.log("isAlready " + isAlreadyFriend);
+            console.log(userFound);
+            //Or a user cannot add themselves as a friend.
+            if(userFound.username != "" && isAlreadyFriend === 0 && userFound.username != aUser.username) {
+            console.log("this person exists, added as a friend");
+
+            aUser.friends.push({
+                    key: userFound.username,
+                    selected: false,
+                    username: userFound.username,
+                    image: userFound.avatar
+                });
+                userFound.push({
+                    key: aUser.username,
+                    selected: false,
+                    username: aUser.username,
+                    image: aUser.avatar
+                });
+            
+            setFriends(aUser.friends.concat(aUser, {key: userFound.username ,selected: false, username: userFound.username, image: userFound.avatar}));
+            await saveList(save, names);
+              }           
+            else {
+              console.log("This person is already a friend");
+            }
+            }
+
+else {
+  console.log("Respons is not ok or this person DNE");
+}
+//get user database
+
+//look for the username, if it exists, inform the user, and add/set it to friends array.
+//if it doesn't inform user about that person DNE with alert. 
+
+}
+
+function searchButton() {
+  console.log("Search Button");
+    setShowme(true);
+  
+  }
+
+
   //simple list object created with Friends js
   var alist = <View style = {styles.container} >
-                <Friend friendList={friends} source={{uri: aphoto}} /> 
+                <Friend friendList={friends} source={{uri: aphoto}} />
+                {dialogInp}
+                <CustomButton name = "Search for a Friend" click ={searchButton} /> 
                 </View>
   return (alist);
 }
